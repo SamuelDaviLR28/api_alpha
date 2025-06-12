d42fa9e1a53171445395cce6b2ceeeb0e30d8db3
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-12 14:36:16 -0300

    Update __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .dispatch import Dispatch
+from .patch import Patch
+from .rastro import Rastro
+from .motorista import Motorista
+from .rota import Rota
+from .cancelamento import Cancelamento
