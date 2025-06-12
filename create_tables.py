ae6b029a056a1a7b89adb8228a0ae842b6c9a4cc
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-12 14:57:17 -0300

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,12 @@
+import asyncio
 from app.database import Base, engine
-from app.models import models  # Importa todos os modelos para registrar as tabelas
+from app.models import models  # Isso garante que todas as classes sejam registradas
 
-print("Criando tabelas no banco de dados...")
-Base.metadata.create_all(bind=engine)
-print("Tabelas criadas com sucesso!")
+async def create_tables():
+    print("Criando tabelas no banco de dados...")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    print("Tabelas criadas com sucesso!")
+
+if __name__ == "__main__":
+    asyncio.run(create_tables())
