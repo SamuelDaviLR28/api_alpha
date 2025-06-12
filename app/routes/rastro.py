741432790e4768d3c97c11b67993d77564972516
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-12 12:33:43 -0300

    Update rastro.py

--- app/routes/rastro.py
+++ app/routes/rastro.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
-from app.models.models import Rastro
+from app.models import rastro
 
 router = APIRouter()
 
