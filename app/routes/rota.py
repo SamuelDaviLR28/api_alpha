e3f486cf2db620f3b1f6b50b6cbb4b78a5556fef
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-17 16:08:30 -0300

    Update rota.py

--- app/routes/rota.py
+++ app/routes/rota.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models import Rota
-from app.schemas.rota import RotaPayload
+from app.schemas import rota
 
 router = APIRouter()
 
