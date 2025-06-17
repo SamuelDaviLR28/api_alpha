fa4aee0c883dc9b3d16bb8ba22aeab27b4cce324
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-17 11:25:35 -0300

    Update cancelamento.py

--- app/routes/cancelamento.py
+++ app/routes/cancelamento.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import Any
 from app.database import SessionLocal
 from app.models import Cancelamento
 
@@ -13,9 +14,12 @@ def get_db():
         db.close()
 
 @router.put("/cancelar-suspender")
-def cancelar_suspender(payload: dict, db: Session = Depends(get_db)):
+def cancelar_suspender(payload: Any, db: Session = Depends(get_db)):
     novo = Cancelamento(dados=payload)
     db.add(novo)
     db.commit()
     db.refresh(novo)
-    return {"message": "Cancelamento ou suspensão processado", "id": novo.id}
+    return {
+        "message": "Cancelamento ou suspensão processado",
+        "id": novo.id
+    }
