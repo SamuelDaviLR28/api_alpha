dc0bc4ab206060e78d5ff7f32351b0dbe7faa71e
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-23 18:12:39 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,36 +1,25 @@
-from fastapi import FastAPI, Header, HTTPException, Depends
-from dotenv import load_dotenv
-import os
+class DispatchToutbox(BaseModel):
+    CriacaoPedido: Optional[str] = None
+    DataPagamento: Optional[str] = None
+    NumeroPedido: Optional[str] = None
+    NumeroPedidomarketplace: Optional[str] = None
+    NumeroPedidoMarketplace: Optional[str] = None
+    NumeroPedidoErp: Optional[str] = None
+    NumeroPedidoAux: Optional[str] = None
+    IdsAuxiliares: Optional[str] = None
 
-from app.routes import dispatch, patch, rastro, motorista, rota, cancelamento
-from app.database import engine, Base
+    Marketplace: Optional[Marketplace] = None
+    Marca: Optional[Marca] = None
+    Seller: Optional[Seller] = None
 
-load_dotenv()
+    CanalDeVenda: Optional[CanalDeVenda] = None
+    Warehouse: Optional[str] = None
+    UnidadeDeNegocio: Optional[str] = None
+    Rede: Optional[str] = None
+    Campanha: Optional[str] = None
 
-app = FastAPI()
+    Itens: Optional[List[Item]] = None
+    NotaFiscal: Optional[NotaFiscal] = None
+    InfosAdicionais: Optional[InfosAdicionais] = None
 
-API_KEY = os.getenv("API_KEY")
-
-
-async def verify_api_key(x_api_key: str = Header(...)):
-    if x_api_key != API_KEY:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-
-
-@app.get("/")
-async def root():
-    return {"message": "API rodando online com sucesso!"}
-
-
-app.include_router(dispatch.router, dependencies=[Depends(verify_api_key)])
-app.include_router(patch.router, dependencies=[Depends(verify_api_key)])
-app.include_router(rastro.router, dependencies=[Depends(verify_api_key)])
-app.include_router(motorista.router, dependencies=[Depends(verify_api_key)])
-app.include_router(rota.router, dependencies=[Depends(verify_api_key)])
-app.include_router(cancelamento.router, dependencies=[Depends(verify_api_key)])
-
-
-@app.on_event("startup")
-async def startup_event():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    model_config = {"extra": "allow"}
