b14144304c6a02c1cd449c47f9b451398dbd115b
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-24 15:27:48 -0300

    Update dispatch.py

--- app/schemas/dispatch.py
+++ app/schemas/dispatch.py
@@ -199,5 +199,8 @@ class DispatchToutbox(MeuBaseModel):
     VersaoSchema: Optional[str] = "v2.11.3"
 
     def __init__(__pydantic_self__, **data):
-    print("🧠 [DEBUG] Instanciando DispatchToutbox de:", __pydantic_self__.__module__)
-    super().__init__(**data)
+        print("🧠 [DEBUG] Instanciando DispatchToutbox de:", __pydantic_self__.__module__)
+        super().__init__(**data)
+
+class RotaPayload(DispatchToutbox):
+    pass
